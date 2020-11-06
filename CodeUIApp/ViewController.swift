9673ca598aa738f647749011cbb3e1023f30932f
SettlementApp <aki01191017@gmail.com>
2020-11-07 08:35:48 +0900

    Initial Commit

---
+++ CodeUIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeUIApp
+//
+//  Created by 柿沼儀揚 on 2020/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
